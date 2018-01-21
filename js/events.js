3acedda09830eebd86d3c3f5bf5d1f7e9cedaeba
Lori Kulikowski <soupfairy99@gmail.com>
2018-01-21 18:00:30 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,19 @@
 //define functions here
+functions
+getIt()
+{$D
+
 
 $(document).ready(function(){
 
 // call functions here
 
 });
+/*Define a function `getIt` that does not accept a parameter. The function should bind a `click` event to the `p` tag. When the paragraph is clicked, the function should alert `"Hey!"`.
+
++ Define a function `frameIt` that does not accept a parameter. The function should bind the `load` event that adds the class `tasty` to the image to add a red frame to the image.
+
++ Define a function `pressIt` that does not accept a parameter. The function should bind a `keydown` event to the input field of the form that alerts a user when they have pressed the `G` key.
+
++ Define a function `submitIt` that does not accept a parameter. The function should bind a `submit` event to the form that alerts `"Your form is going to be submitted now."`.
+*/
